ba7874292b0c91d3007ca11936ccc45c7b185528
Víctor Yrazusta <victoryrazustaibarra@gmail.com>
2019-09-10 17:36:01 +0200

    Hello World cambiado a Hola Mundo

--- hello.py
+++ hello.py
@@ -4,9 +4,8 @@ def main():
     if len(sys.argv) >= 2:
         name = sys.argv[1]
     else:
-        name = 'World'
-    print('Hello', name)
-
+        name = 'Mundo'
+    print('Hola', name)
 
 if __name__ == '__main__':
     main()
